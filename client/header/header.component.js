e2ba5acff5d8938380396fefdaa852eb17205607
Micah Wells <micahp0506@gmail.com>
2016-08-14 17:02:51 -0500

    Trying to get app to serve

--- client/header/header.component.js
+++ client/header/header.component.js
@@ -1,11 +1,13 @@
-// Register `header` component, along with its associated controller and template
+'use strict';
+
+
 angular.
   module('header').
   component('header', {
     templateUrl: 'header/header.html',
     controller: ['Header',
       function HeaderController() {
-        console.log("poop");
+        console.log("this", this);
       }
     ]
   });
